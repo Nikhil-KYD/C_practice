f19b191cd88198fcf6b3d845443a9911800deac5
Nikhil-git <voyagerelev@gmail.com>
2026-09-23 16:29:02 +0530

    updates

---
+++ ArrayP/aa.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+    int numb[1];
+
+    printf("Enter a number: ");
+    scanf("%d", &numb[0]);
+
+    printf("Square of %d is %d\n", numb[0], numb[0] * numb[0]);
+
+    return 0;
+}
